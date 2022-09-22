d78f0be17850eba044eebbb42d85ca589d35e6bb
Catherine Omego <omegocatherine@gmail.com>
2022-09-22 14:21:19 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -24,14 +24,14 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[counter] != '\0')
 	{
-			destlen++;
+		destlen++;
 
-			counter++;
+		counter++;
 	}
-			for (i = 0; i < n && src[i] != '\0'; i++)
-				dest[destlen + i] = src[i];
+		for (i = 0; i < n && src[i] != '\0'; i++)
+			dest[destlen + i] = src[i];
 
-			dest[destlen + i] = '\0';
+		dest[destlen + i] = '\0';
 
-			return (dest);
+		return (dest);
 }
