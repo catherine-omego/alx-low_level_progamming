d76e4d0a8389cdc6ed0e4b93d63965457cf42984
Catherine Omego <omegocatherine@gmail.com>
2022-09-26 18:22:00 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /*
- * _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * _memset - fills the first n bytes of the memory area pointed to by s
+ *
+ * with the constant byte b
  *
  * @s: to be filled with a buffer
  *
