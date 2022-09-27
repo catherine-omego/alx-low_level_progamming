ed3d07ec48014c3de980772e2fdd3bbbf55d98b8
Catherine Omego <omegocatherine@gmail.com>
2022-09-27 04:16:58 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char number = '0';
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
