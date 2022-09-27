c0c953d116599cb0ab45a1decb35cc41040a4478
Catherine Omego <omegocatherine@gmail.com>
2022-09-27 04:39:03 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int d1, d2, n = 0;
+
+	for (d1 = '0'; d1 <= '9'; d1++)
+	{
+	for (d2 = d1 + 1; d2 <= '9'; d2++, n++)
+	{
+		if (n > 0)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(d1);
+		putchar(d2);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
