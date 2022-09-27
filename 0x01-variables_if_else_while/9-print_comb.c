2471e53074239bf1d7885b29a682c150cdf5f6ab
Catherine Omego <omegocatherine@gmail.com>
2022-09-27 04:32:49 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int item;
+
+	for (item = '0'; item <= '9'; item++)
+	{
+		putchar(item);
+
+		if (item != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
