edbaf0b339cb7b2ab9af400b86375c741573d516
Catherine Omego <omegocatherine@gmail.com>
2022-10-05 04:44:10 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: sum of a & b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
