4ebd997140fac00a4ae5cd3d40ab2f833f9000ff
Catherine Omego <omegocatherine@gmail.com>
2022-10-04 18:10:09 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: the real number
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+
+	return (n);
+
+	else if (n < 0)
+
+	return (n * -1);
+
+	else
+
+	return (0);
+}
