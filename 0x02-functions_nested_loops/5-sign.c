3938119e45baf6a24575870837c37a2487a29827
Catherine Omego <omegocatherine@gmail.com>
2022-10-04 18:05:28 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - entry
+ *
+ * @n: integer
+ *
+ * Return: 1 if n is positive, -1 if n is negative and 0 if n is zero
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	return (0);
+	}
+
+	else if (n > 0)
+	{
+		_putchar('+');
+	return (1);
+	}
+	else
+	{
+		_putchar('-');
+	return (-1);
+	}
+}
