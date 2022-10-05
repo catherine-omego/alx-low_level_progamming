bbe12f5d55f9bbe4fdf49ba4313b00cf61a4a108
Catherine Omego <omegocatherine@gmail.com>
2022-10-04 18:13:19 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @number: the number to be printed
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int number)
+{
+	return (number % 100);
+}
