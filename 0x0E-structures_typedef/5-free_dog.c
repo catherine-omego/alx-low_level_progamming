60415d28f931be872f91709d353bdf2895414234
Catherine Omego <omegocatherine@gmail.com>
2022-10-10 19:41:58 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees a dog
+ *
+ * @d: A dog struct
+ *
+ * Return: Null
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
