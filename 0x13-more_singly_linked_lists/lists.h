86f35e6d408ecbdc03c3cd297688fc477df14c64
Catherine Omego <omegocatherine@gmail.com>
2022-10-24 18:26:14 -0700

    header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * struct listint_s - struct for the project
+ *
+ * @n: data of type int
+ *
+ * @next: link to the next node
+ *
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 int _putchar(char);
 size_t print_listint(const listint_t *h);
